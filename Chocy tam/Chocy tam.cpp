726665204f93eaf9f4cdc8dca9ca61f687f2393b
admin1 <admin1@ADMIN1-PC36>
2022-07-22 15:54:33 +0300

    Добавьте файлы проекта.

---
+++ Chocy tam/Chocy tam.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+unsigned int greatest_common_divisor(unsigned int a, unsigned int b) {
+    if (a == b)
+        return a;
+    if (a > b)
+        return greatest_common_divisor(a - b, b);
+    return greatest_common_divisor(a, b - a);
+}
+unsigned int NCK(unsigned int a, unsigned int b) 
+{
+    if (a % 2 == 0 && b % 2 == 0) return 2;
+    if (a % 3 == 0 && b % 3 == 0) return 3;
+    if (a % 5 == 0 && b % 5 == 0) return 5;
+    return 1;
+}
+
+
+int main()
+{
+    int a = 10, b = 9;
+    cout << greatest_common_divisor(a, b) << endl;
+    cout << NCK(a, b);
+    return 0;
+}
